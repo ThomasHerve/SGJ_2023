d398b86bf0f2c152667f3167766699c23ebe3a58
Fx73 <fx73000@yahoo.fr>
2023-02-15 15:07:53 +0100

    Unity Base Project

---
+++ Assets/Sources/Menu/MenuNavigation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Unity.MultiPlayerGame.Shared;
+
+namespace Unity.MultiPlayerGame.Menu
+{
+    public class MenuNavigation : MonoBehaviour
+    {
+        public Selectable DefaultSelection;
+
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+
+        void LateUpdate()
+        {
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                if (Input.GetButtonDown(GameConstants.k_ButtonNameSubmit)
+                    || Input.GetAxisRaw(GameConstants.k_AxisNameHorizontal) != 0
+                    || Input.GetAxisRaw(GameConstants.k_AxisNameVertical) != 0)
+                {
+                    EventSystem.current.SetSelectedGameObject(DefaultSelection.gameObject);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# MultiPlayer_Menu_Template
+<h1>MultiPlayer Menu Template</h1>
+
+![IM2AG](https://img.shields.io/badge/IM2AG-Seal%20of%20Quality-blue)
+![GitHub last commit](https://img.shields.io/github/last-commit/fx73/MultiPlayer_Menu_Template)
+
+<h2>Presentation</h2>
+
+Menu template for a Unity Multiplayer Game
+
+<h2>How it works</h2>
+
+Fork it !
+
+
+<br>
+
