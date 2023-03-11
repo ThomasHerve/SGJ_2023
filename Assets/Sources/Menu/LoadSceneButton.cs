c60407f49421a1f39d8f3f7cc9e570e279308304
Fx73 <fx73000@yahoo.fr>
2023-03-11 01:18:05 +0100

    Scene change fix

--- Assets/Sources/Menu/LoadSceneButton.cs
+++ Assets/Sources/Menu/LoadSceneButton.cs
@@ -9,15 +9,6 @@ namespace Unity.MultiPlayerGame.Menu
     {
         public string SceneName = "";
 
-        void Update()
-        {
-            if (EventSystem.current.currentSelectedGameObject == gameObject
-                && Input.GetButtonDown(GameConstants.k_ButtonNameSubmit))
-            {
-                LoadTargetScene();
-            }
-        }
-
         public void LoadTargetScene()
         {
             SceneManager.LoadScene(SceneName);
