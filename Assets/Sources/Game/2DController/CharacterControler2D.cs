5186b6309c5ed85434a443a6d543143cf8a98e0e
Zarukin <benoitsonz@gmail.com>
2023-03-11 19:59:49 +0100

    ajout background

--- Assets/Sources/Game/2DController/CharacterControler2D.cs
+++ Assets/Sources/Game/2DController/CharacterControler2D.cs
@@ -110,7 +110,7 @@ public class CharacterControler2D : MonoBehaviour
             {
                 cd = Time.time;
                 //Tirer
-                GameObject g = GameObject.Instantiate(balle, Canon.transform.position, Quaternion.Euler(0, 0, 0));
+                GameObject g = GameObject.Instantiate(balle, new Vector3(Canon.transform.position.x, Canon.transform.position.y,1), Quaternion.Euler(0, 0, 0));
                 g.transform.SetParent(gameObject.transform);
                 ondes o = g.GetComponent<ondes>();
   
